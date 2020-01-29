21e2a199524ac54efed755b02a830dd1d9a43268
Helder Calazans <heldercalazanss@gmail.com>
2020-01-28 21:46:48 -0300

    Adicionando função para recuperar o link do documento

--- js/documentos.js
+++ js/documentos.js
@@ -8,24 +8,43 @@ function validation(cpfValue){
     var storage = firebase.storage();
     storage.ref().child(cpfValue).listAll().then(function(todosArquivos){
         if(todosArquivos.items.length >= 1){
+            listFiles(cpfValue);
             next(cpfValue);
-            alert("arquivo " + todosArquivos.items[0].name + " encontrado");
         }
         else{
             alert("CPF não cadastrado");
         }
-        console.log(todosArquivos.items[0].name);
+        
 
     }).catch(function(error){
         console.log(error);
     }); // Retorna uma Promise
 }
 
+function listFiles(cpfValue){
+
+    document.getElementById('tituloDocumentos').innerHTML = 'Certificados de: '+cpfValue;
+    var files;
+    var fileNames = [];
+    var fileLinks = [];
+    var storage = firebase.storage();
+
+    storage.ref().child(cpfValue).listAll().then(function(listaArquivos){
+        files = listaArquivos.items;
+        console.log(files);
+        for(let i = 0; i<files.length; i++){
+            fileNames.push(files[i].name);
+            storage.ref(cpfValue+'/'+fileNames[i]).getDownloadURL().then(function(url){
+                console.log(url);
+            });
+        }
+    });
+}
+
 function next(cpfValue){
 
     document.getElementById('busca').setAttribute("class", "ocultar");
     document.getElementById('resultado').removeAttribute("class", "ocultar");
-    document.getElementById('tituloDocumentos').innerHTML = 'Certificados de: '+cpfValue;
 }
 
 function back(){
